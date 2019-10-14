aad0c7ea2fabf207d7b6deef1aaff285be33e424
g33kism <gunrajsm@gmail.com>
2019-10-14 16:12:15 -0400

    initialize

---
+++ src/main/java/com/g33kism/rxfilewatcher/RxFilewatcherApplication.java
@@ -0,0 +1,13 @@
+package com.g33kism.rxfilewatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxFilewatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxFilewatcherApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/g33kism/rxfilewatcher/watchers/FileWatcher.java
@@ -0,0 +1,4 @@
+package com.g33kism.rxfilewatcher.watchers;
+
+public class FileWatcher {
+}
